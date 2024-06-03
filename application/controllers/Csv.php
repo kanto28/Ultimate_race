65f55b9ef48556b450dc6b811b029e25a2c1e65c
kanto28 <kantoandrianaivosoa28@gmail.com>
2024-06-03 14:00:45 +0300

    import_point

--- application/controllers/Csv.php
+++ application/controllers/Csv.php
@@ -9,24 +9,30 @@ class Csv extends CI_Controller {
     }
 
    
+    //import des points
     public function import_point() {
-        if ($_FILES['point_csv']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['point_csv']['tmp_name'])) {
+
+		if($_FILES['point_csv']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['point_csv']['tmp_name']))
+        {
             $point_csv = $_FILES['point_csv']['tmp_name'];
-            if (($handle = fopen($point_csv, "r")) !== FALSE) {
-                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                    foreach ($data as &$value) {
-                        $value = str_replace(',', '.', $value);
-                    }
+            if(($handle = fopen($point_csv, "r")) !== FALSE)
+            {
+                while(($data = fgetcsv($handle ,1000, "," )) !== FALSE )
+                {
+					foreach ($data as &$value) {
+						$value = str_replace(',', '.', $value);
+                        $value = str_replace('/', '-', $value);
+					}
                     $this->Csv_model->insert_point($data);
                 }
                 fclose($handle);
             }
         }
-        $this->Csv_model->insert_all_data_point();
-        redirect('Admin/dashboard'); /
-    }
+		
+		$this->Csv_model->insert_all_data_point();
+        redirect('Admin/dashboard');
+	}
 
-	
 
 	
 
--- application/models/Csv_model.php
+++ application/models/Csv_model.php
@@ -5,28 +5,39 @@ if ( ! defined('BASEPATH')) exit('No direct script acces allowed');
 
 class Csv_model extends CI_Model{
    
-    
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Point_model');
+    }
+
     public function insert_point($data) {
         $data_insert = array(
             'rang' => $data[0],
             'points' => $data[1]
         );
+        //var_dump($data_insert);
         $this->db->insert('table_point_ref', $data_insert);
+        $this->db->insert_id();
     }
+    
 
     public function insert_all_data_point() {
         $query = $this->db->get('table_point_ref');
         $donnee = $query->result();
 
-        foreach ($donnee as $key => $value) {
+        foreach ($donnee as $key => $value)
+        {
+            
             $data = array(
                 'rang' => $value->rang,
                 'points' => $value->points
+
             );
+
+            $this->Point_model->insert_point($data);
+
         }
     }
 
 
-
- 
 }
