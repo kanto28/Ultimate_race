2a3cf56e66e68bc77cb4b453293f1f546d482af9
kanto28 <kantoandrianaivosoa28@gmail.com>
2024-06-03 15:48:02 +0300

    csv_etape

--- application/controllers/Csv.php
+++ application/controllers/Csv.php
@@ -34,6 +34,52 @@ class Csv extends CI_Controller {
 	}
 
 
+    //import des etape et participation
+    public function import_resultat() {
+
+		if($_FILES['etape_csv']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['etape_csv']['tmp_name']))
+        {
+            $etape_csv = $_FILES['etape_csv']['tmp_name'];
+            if(($handle = fopen($etape_csv, "r")) !== FALSE)
+            {
+                while(($data = fgetcsv($handle ,1000, "," )) !== FALSE )
+                {
+					foreach ($data as &$value) {
+						$value = str_replace(',', '.', $value);
+					}
+                    $this->Csv_model->insert_etape($data);
+                }
+                fclose($handle);
+            }
+        }
+
+		if($_FILES['participation_csv']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['participation_csv']['tmp_name']))
+        {
+            $participation_csv = $_FILES['participation_csv']['tmp_name'];
+            if(($handle = fopen($participation_csv, "r")) !== FALSE)
+            {
+                while(($data = fgetcsv($handle ,1000, "," )) !== FALSE )
+                {
+					foreach ($data as &$value) {
+						$value = str_replace(',', '.', $value);
+						$value = str_replace('%', '', $value);
+                        $value = str_replace('/', '-', $value);
+					}
+                    
+                    $this->Csv_model->insert_Participation($id_coureur, $id_etape, $heure_depart, $penalite = 0);
+					
+                }
+                fclose($handle);
+            }
+        }
+
+		$this->Csv_model->insert_all_data_etape();
+		$this->Csv_model->insert_all_data_participation();
+        redirect('Admin/dashboard');
+	}
+
+	
+
 	
 
 }
